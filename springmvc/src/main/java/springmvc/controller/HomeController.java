70500e7a250a1016624451dd443116a3eb55cfd0
Pratham <pratham.dhobi.softsages@gmail.com>
2024-01-02 11:37:38 +0530

    Learned Model class for adding the values

--- springmvc/src/main/java/springmvc/controller/HomeController.java
+++ springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -1,14 +1,24 @@
 package springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
         System.out.println("This is home url");
+        model.addAttribute("name","Pratham pratham pratham");
+       model.addAttribute("id",121212);
+       List<String> students = new ArrayList<String>();
+       students.add("pratham");
+       students.add("dhyana");
+       model.addAttribute("names",students);
+
         return "index";
     }
 }
-//this is added
--- springmvc/src/main/webapp/WEB-INF/views/index.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,5 +1,21 @@
+
+<!DOCTYPE html>
+<%@ page import="java.util.List" %>
 <html>
 <body>
 <h2>Hello World! pratham here</h2>
+<%
+     String s = (String) request.getAttribute("name");
+     Integer id = (Integer) request.getAttribute("id");
+     List<String> names = (List<String>) request.getAttribute("names");
+%>
+<h1> name of the pratham is <%=s%> and id is <%=id%>  </h1>
+<%
+if(names!=null) {for(String str : names){
+%>
+ <h2><%=str%></h2>
+<%
+   }  }
+%>
 </body>
 </html>
