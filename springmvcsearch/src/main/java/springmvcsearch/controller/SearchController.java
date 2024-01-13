a96c5b1077868809acd455eab6c83ce724590312
Your Name <you@example.com>
2024-01-13 11:30:26 +0530

    static files added

--- springmvcsearch/src/main/java/springmvcsearch/controller/SearchController.java
+++ springmvcsearch/src/main/java/springmvcsearch/controller/SearchController.java
@@ -11,7 +11,7 @@ public class SearchController {
     @RequestMapping("/home")
     public String home(){
         System.out.println("THis is home view...!!");
-        return "index";
+        return "home";
     }
     @RequestMapping("/search")
     public RedirectView search(@RequestParam("querybox") String q){
@@ -22,3 +22,11 @@ public class SearchController {
         return r;
     }
 }
+
+/*serving static files in spring mvc like css,js,img
+ step 1 crete the folders of js,css,img in WEB-INF/resources/**
+ step 2 in the spring-servlet.xml or you can use the java based config as well
+        add spring-mvc in header mvc:resources and annotatino driven add
+step 3 in home.jsp add tablib and tag for  css,js,img but there tablib is not working
+the line 1 of home.jsp is here and i have removed that from there : <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+ */
--- springmvcsearch/src/main/webapp/WEB-INF/views/home.jsp
+++ springmvcsearch/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,3 +1,5 @@
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,10 +9,12 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <link href="<c:url value="/resources/css/style.css"/>" rel="stylesheet"  >
+    <script src="<c:url value="/resources/js/script.js"/>"></script>
     <title>Hello, world!</title>
   </head>
   <body>
+  <img alt="My image" src="<c:url value="/resources/image/WIN_20240113_09_29_55_Pro.jpg" />">
   <div class="container">
   <div class ="card mx-auto mt-5 bg-secondary" style="width: 50%;">
   <div class ="card-body py-5">
